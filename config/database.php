fd12bdb3e4ec084785b459121c3d001bd9e4f09b
Grigory <133101349+Worwulew@users.noreply.github.com>
2023-05-16 17:20:42 +0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'Grigory');
-define('DB_PASS', '12345');
-define('DB_NAME', 'php_dev');
+define('DB_PASS', 'Your_db_password');
+define('DB_NAME', 'Your_db_name');
 
 // create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME); // like we did when created user class and then new user1 obj, but here we created a new connection obj with db
